0abaf60e1e47d45dd17d7f818176dba76ccab93e
fadilparves <fadil.parves@gmail.com>
2020-11-23 22:20:34 +0800

    first graphql endpoint ready

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 from fastapi import FastAPI
+import graphene
+from starlette.graphql import GraphQLApp
+from schema import Query
 
 app = FastAPI()
+
+app.add_route('/graphql', GraphQLApp(schema=graphene.Schema(query=Query)))
 
 @app.get('/')
 def home():
